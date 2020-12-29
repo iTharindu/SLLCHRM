059d6a81d3996e0c3992490273581b37dbe947b0
sandeepa97 <lbsandeepa@gmail.com>
2020-12-29 22:38:59 +0530

    manurawa 2nd commit

---
+++ manurawa/assets/js/custom.js
@@ -0,0 +1,30 @@
+//  =======================================================
+// * Web Name: Manurawa 2020
+// * Web Version: 1.0
+// * Developer: Sandeepa Lokubaranige | www.sandeepa.net
+// * Owner: SLLCHRM | www.sllchrm.lk
+// ======================================================== 
+
+var goBtn = document.getElementById("goBtn");
+var menu = document.getElementById("menu");
+
+goBtn.onclick = function() {
+  window.location = menu.value;
+}
+
+$(document).ready(function(){
+  $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+    
+    $(".zoom").hover(function(){
+		
+		$(this).addClass('transition');
+	}, function(){
+        
+		$(this).removeClass('transition');
+	});
+});
+
+
